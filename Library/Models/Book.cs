4f46ff315cd1d06caa69192c5724e8c833b0174f
Dusty <dustymccord@gmail.com>
2020-03-25 10:51:25 -0700

    add author model and bookAuthor model.
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Dominique Youmans <ddyoumans@icloud.com>
    Co-authored-by: Brandan <brandan@brandan.tech>

--- Library/Models/Book.cs
+++ Library/Models/Book.cs
@@ -1,12 +1,18 @@
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace Library.Models
 
 {
   public class Book
   {
+    public Book()
+    {
+      this.Authors = new HashSet<BookAuthor>();
+    }
     public int BookId { get; set; }
     public string Title { get; set; }
     public int PublishedYear { get; set; }
+    public virtual ICollection<BookAuthor> Authors { get; set; }
   }
 }
--- Library/Models/LibraryContext.cs
+++ Library/Models/LibraryContext.cs
@@ -6,6 +6,8 @@ namespace Library.Models
   public class LibraryContext : IdentityDbContext<ApplicationUser>
   {
     public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<BookAuthor> BookAuthor { get; set; }
 
     public LibraryContext(DbContextOptions options) : base(options) { }
   }
